14444e4090c50c746381b63dd122e9d3f32202da
Wene Alves <weneplay5@gmail.com>
2020-11-11 11:09:09 -0300

    Sincronizando e Recriando as Tabelas e Relacionando as tabelas

--- articles/Article.js
+++ articles/Article.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize')
 const connection = require('../database/database')
+const Category = require('../categories/Category')
 const Article = connection.define('articles',{
     title: {
         type: Sequelize.STRING,
@@ -14,5 +15,12 @@ const Article = connection.define('articles',{
         allowNull: false
     }
 })
+
+// relacionamentos entre tables
+Category.hasMany(Article) // 1 para Muitos
+Article.belongsTo(Category)  // 1 para 1
+
+// Sincronizar e Recriar Tables
+Article.sync({ force: true })
 
 module.exports = Article
