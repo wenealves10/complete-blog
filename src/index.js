12970d9592d3923c6f54521a7aa664109c03342a
Wene Alves <weneplay5@gmail.com>
2020-11-11 09:34:41 -0300

    fazendo a conexão de banco de dados no index principal do projeto

--- src/index.js
+++ src/index.js
@@ -2,20 +2,34 @@ require('dotenv/config')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const connection = require('../database/database')
 
+// objeto de configuração da aplicação
 const configs = {
     port: process.env.PORT || 8080,
 }
 
+// configurando o express
 app.set('view engine','ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
+// conexão com o banco de dados
+connection
+  .authenticate()
+     .then((result) => {
+         console.log('connection success!')
+     }).catch((err) => {
+         console.log('connection erro: '+err)
+     });
+
+// rotas gets da aplicação 
 app.get('/',(req, res) =>{
     res.render('index')
 })
 
+// porta da aplicação
 app.listen(configs.port, () => {
     console.log(`Server port ${configs.port}`)
 })
