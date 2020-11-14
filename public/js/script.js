5f21ab2755e93026b4df4812b1dbc839d0af7192
Wene Alves <weneplay5@gmail.com>
2020-11-14 18:08:09 -0300

    retirando configs padrão

--- public/js/script.js
+++ public/js/script.js
@@ -9,6 +9,7 @@ $(function(){
 
 tinymce.init({
     selector: '#article',
+    language:'pt_BR',
     plugins: [
       'advlist autolink link image lists charmap print preview hr anchor pagebreak',
       'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
@@ -16,10 +17,7 @@ tinymce.init({
     ],
     toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
       'bullist numlist outdent indent | link image | print preview media fullpage | ' +
-      'forecolor backcolor emoticons | help',
-    menu: {
-      favs: {title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons'}
-    },
+      'forecolor backcolor emoticons',
     menubar: 'favs file edit view insert format tools table help',
     content_css: 'css/content.css'
   });
