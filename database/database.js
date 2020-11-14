b5afa3fb6758e1edd5c69a27733739bfe94fd3a5
Wene Alves <weneplay5@gmail.com>
2020-11-14 18:08:36 -0300

    adicionando time zone  do brasil no banco de dados

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require('sequelize')
 
 const connection = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 })
 
 module.exports = connection
