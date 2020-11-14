0df76942c98ffb1c27a2dcc30cb301f3e8164331
Wene Alves <weneplay5@gmail.com>
2020-11-14 11:29:52 -0300

    Estilos avançados do tinymce

--- public/js/script.js
+++ public/js/script.js
@@ -8,7 +8,18 @@ $(function(){
 });
 
 tinymce.init({
-    selector: 'textarea#article', 
-    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent'
+    selector: '#article',
+    plugins: [
+      'advlist autolink link image lists charmap print preview hr anchor pagebreak',
+      'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+      'table emoticons template paste help'
+    ],
+    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+      'bullist numlist outdent indent | link image | print preview media fullpage | ' +
+      'forecolor backcolor emoticons | help',
+    menu: {
+      favs: {title: 'My Favorites', items: 'code visualaid | searchreplace | emoticons'}
+    },
+    menubar: 'favs file edit view insert format tools table help',
+    content_css: 'css/content.css'
   });
-  
