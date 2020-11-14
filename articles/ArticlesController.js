b355b122ba7d22d71a28c4553d951c0ee8383aa8
Wene Alves <weneplay5@gmail.com>
2020-11-14 18:09:08 -0300

    Rota de criação de artigo no banco de dados

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,12 +1,43 @@
 const express = require('express')
 const router = express.Router()
+const Category = require('../categories/Category')
+const Article = require('./Article')
+const slugify = require('slugify')
 
 router.get('/admin/articles',(req, res) =>{
     res.send('Rota de Artigos')
 })
 
 router.get('/admin/articles/new',(req, res) =>{
-    res.render('admin/articles/new')
+    Category.findAll().then(categories =>{
+        res.render('admin/articles/new',{
+            categories
+        })
+    })
 })
+
+router.post('/articles/save',(req, res) =>{
+    let categoryId = req.body.category
+    let articleTitle = req.body.title
+    let articleBody = req.body.body
+
+    if(articleTitle != undefined && articleBody != undefined){
+        Article.create({
+            title: articleTitle,
+            slug: slugify(articleTitle,{
+                lower: true
+            }),
+            body: articleBody,
+            categoryId,
+        }).then(() =>{
+            res.redirect('/')
+        })
+    }else{
+        res.redirect('/admin/articles/new')
+    }
+
+})
+
+
 
 module.exports = router
