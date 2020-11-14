3ac9a010f040573b662fcdd5e93e4409096c282f
Wene Alves <weneplay5@gmail.com>
2020-11-14 11:18:59 -0300

    estilizando o tinymce

--- public/js/script.js
+++ public/js/script.js
@@ -8,10 +8,7 @@ $(function(){
 });
 
 tinymce.init({
-    selector: 'textarea',
-    plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-    toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table',
-    toolbar_mode: 'floating',
-    tinycomments_mode: 'embedded',
-    tinycomments_author: 'Author name',
+    selector: 'textarea#article', 
+    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | outdent indent'
   });
+  
