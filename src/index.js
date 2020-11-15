87613851b15dcc27c1fee4d4be09d131cebb0759
Wene Alves <weneplay5@gmail.com>
2020-11-15 15:48:06 -0300

    fitragem de categorias

--- src/index.js
+++ src/index.js
@@ -77,6 +77,30 @@ app.get('/:slug', (req, res) => {
     })
 })
 
+app.get('/categories/:slug',(req, res) =>{
+    let slug = req.params.slug
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{model: Article}]
+    }).then(category =>{
+        if(category != undefined){
+            Category.findAll()
+                .then(categories =>{
+                    res.render('index',{
+                        articles: category.articles,
+                        categories
+                    })
+                })
+        }else{
+            res.redirect('/')
+        }
+    }).catch(err =>{
+        res.redirect('/')
+    })
+})
+
 // rotas categorias
 app.use('/', categoriesController)
 
