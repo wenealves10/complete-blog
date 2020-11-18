c594eef7e09ec11a0e13e870160b5836e50f730c
Wene Alves <weneplay5@gmail.com>
2020-11-18 08:22:40 -0300

    model de usuários

---
+++ users/Users.js
@@ -0,0 +1,17 @@
+const Sequelize = require('sequelize')
+const connection = require('../database/database')
+
+const Users = connection.define('users',{
+    email: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
+
+// Users.sync({force: true})
+
+module.exports = Users
