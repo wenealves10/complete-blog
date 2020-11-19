d3ac7ef2816a6d132a28131f50ae6b19e2442b48
Wene Alves <weneplay5@gmail.com>
2020-11-19 10:20:23 -0300

    oganizando o código

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -5,64 +5,70 @@ const Article = require('./Article')
 const slugify = require('slugify')
 
 
-router.get('/admin/articles',(req, res) =>{
+router.get('/admin/articles', (req, res) => {
     Article.findAll({
-        include: [{model: Category}]
-    }).then(articles =>{
-        res.render('admin/articles/index',{
+        include: [{
+            model: Category
+        }]
+    }).then(articles => {
+        res.render('admin/articles/index', {
             articles
         })
     })
 })
 
-router.get('/admin/articles/new',(req, res) =>{
-    Category.findAll().then(categories =>{
-        res.render('admin/articles/new',{
+router.get('/admin/articles/new', (req, res) => {
+    Category.findAll().then(categories => {
+        res.render('admin/articles/new', {
             categories
         })
     })
 })
 
-router.get('/admin/articles/edit/:id',(req, res) =>{
+router.get('/admin/articles/edit/:id', (req, res) => {
     let id = req.params.id
-    if(!isNaN(id) && id != undefined){
+    if (!isNaN(id) && id != undefined) {
         Article.findByPk(id)
-        .then(articles =>{
-            Category.findAll()
-                .then(categories =>{
-                    res.render('admin/articles/edit',{
-                        articles,
-                        categories
+            .then(articles => {
+                Category.findAll()
+                    .then(categories => {
+                        res.render('admin/articles/edit', {
+                            articles,
+                            categories
+                        })
                     })
-                })
-        })
-    }else{
+            })
+    } else {
         res.redirect('/admin/articles')
     }
 })
 
-router.get('/articles/page/:number',(req, res) =>{
+router.get('/articles/page/:number', (req, res) => {
     let page = req.params.number
     let offset = 0
 
-    if(isNaN(page) || page == 1){
+    if (isNaN(page) || page == 1) {
         offset = 0
-    }else{
+    } else {
         offset = (parseInt(page) - 1) * 4
     }
 
     Article.findAndCountAll({
         limit: 4,
         offset: offset,
-        include: [{model: Category}],
-        order:[['id','DESC']]
+        include: [{
+            model: Category
+        }],
+        order: [
+            ['id', 'DESC']
+        ]
     }).then(articles => {
 
         let next = false
 
-        if(offset + 4 >= articles.count){
+        if (offset + 4 >= articles.count) {
             next = false
-        }else{
+        } else {
             next = true
         }
 
@@ -71,10 +77,10 @@ router.get('/articles/page/:number',(req, res) =>{
             articles,
             next
         }
-        
+
         Category.findAll()
-            .then(categories =>{
-                res.render('admin/articles/pages',{
+            .then(categories => {
+                res.render('admin/articles/pages', {
                     result,
                     categories
                 })
@@ -85,71 +91,73 @@ router.get('/articles/page/:number',(req, res) =>{
 })
 
 
-router.post('/articles/save',(req, res) =>{
+router.post('/articles/save', (req, res) => {
     let categoryId = req.body.category
     let articleTitle = req.body.title
     let articleBody = req.body.body
     let sinopse = req.body.sinopse
+    let thumbnail = 'images.jpg'
 
-    if(articleTitle != undefined && articleBody != undefined && sinopse != undefined){
+    if (articleTitle != undefined && articleBody != undefined && sinopse != undefined) {
         Article.create({
             title: articleTitle,
-            slug: slugify(articleTitle,{
+            slug: slugify(articleTitle, {
                 lower: true
             }),
             body: articleBody,
             sinopse,
+            thumbnail,
             categoryId,
-        }).then(() =>{
+        }).then(() => {
             res.redirect('/admin/articles')
         })
-    }else{
+    } else {
         res.redirect('/admin/articles/new')
     }
 
 })
 
-router.post('/articles/delete',(req, res) =>{
+router.post('/articles/delete', (req, res) => {
     let id = req.body.id
-    
-    if(!isNaN(id) && id != undefined){
+
+    if (!isNaN(id) && id != undefined) {
         Article.destroy({
             where: {
                 id
             }
-        }).then(_ =>{
+        }).then(_ => {
             res.redirect('/admin/articles')
         })
-    }else{
+    } else {
         res.redirect('/admin/articles')
     }
 })
 
-router.post('/articles/edit',(req, res) =>{
+router.post('/articles/edit', (req, res) => {
     let id = req.body.id
     let title = req.body.title
     let body = req.body.body
     let sinopse = req.body.sinopse
     let categoryId = req.body.category
-    if(!isNaN(id) && id != undefined){
+    if (!isNaN(id) && id != undefined) {
 
         Article.update({
             title: title,
-            slug: slugify(title,{
+            slug: slugify(title, {
                 lower: true
             }),
             body: body,
             sinopse: sinopse,
             categoryId: categoryId
-        },{
+        }, {
             where: {
                 id: id
             }
-        }).then(_ =>{
+        }).then(_ => {
             res.redirect('/admin/articles')
         })
-        
-    }else{
+
+    } else {
         res.redirect('/admin/articles')
     }
 })
