f7ff16e7a8ea8847bb7bf0463056851568257da9
Wene Alves <weneplay5@gmail.com>
2020-11-21 17:51:36 -0300

    organizando o código

--- public/js/script.js
+++ public/js/script.js
@@ -1,31 +1,33 @@
 function sendEdit(nameForm) {
-    let form = document.querySelector(nameForm)
-    form.submit()
+  let form = document.querySelector(nameForm)
+  form.submit()
 }
 
 
 
-$(function(){
-    $(document).on('click','input[type=text]',function(){ this.select()})
-    $(document).on('click','#nv > a,#ft > a ',function (e) {
-      e.preventDefault();
-      let href = $(this).attr('href')
-      let setScroll = $(href).offset().top
-      console.log(href)
-      $('html,body').animate({
-        'scrollTop': setScroll
-      },1000)
+$(function () {
+  $(document).on('click', 'input[type=text]', function () {
+    this.select()
+  })
+  $(document).on('click', '#nv > a,#ft > a ', function (e) {
+    e.preventDefault();
+    let href = $(this).attr('href')
+    let setScroll = $(href).offset().top
+    console.log(href)
+    $('html,body').animate({
+      'scrollTop': setScroll
+    }, 1000)
 
-      return false
-    })
-    $(document).on('change','input[type=file]',function(){
-      const file = $(this)[0].files[0]
-      const fileReader = new FileReader()
-      fileReader.onloadend = function(){
-        $('#uploadImage').attr('src',fileReader.result)
-      }
-      fileReader.readAsDataURL(file)
-    })
+    return false
+  })
+  $(document).on('change', 'input[type=file]', function () {
+    const file = $(this)[0].files[0]
+    const fileReader = new FileReader()
+    fileReader.onloadend = function () {
+      $('#uploadImage').attr('src', fileReader.result)
+    }
+    fileReader.readAsDataURL(file)
+  })
 });
 
 Holder.addTheme('thumb', {
@@ -35,16 +37,16 @@ Holder.addTheme('thumb', {
 });
 
 tinymce.init({
-    selector: '#article',
-    language:'pt_BR',
-    plugins: [
-      'advlist autolink link image lists charmap print preview hr anchor pagebreak',
-      'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
-      'table emoticons template paste help'
-    ],
-    toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
-      'bullist numlist outdent indent | link image | print preview media fullpage | ' +
-      'forecolor backcolor emoticons',
-    menubar: 'favs file edit view insert format tools table help',
-    content_css: 'css/content.css',
-  });
+  selector: '#article',
+  language: 'pt_BR',
+  plugins: [
+    'advlist autolink link image lists charmap print preview hr anchor pagebreak',
+    'searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking',
+    'table emoticons template paste help'
+  ],
+  toolbar: 'undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | ' +
+    'bullist numlist outdent indent | link image | print preview media fullpage | ' +
+    'forecolor backcolor emoticons',
+  menubar: 'favs file edit view insert format tools table help',
+  content_css: 'css/content.css',
+});
